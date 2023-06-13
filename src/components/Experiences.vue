81675fa57133302d74ec9c52ac0642d63b19ff97
Théo Grosjean <theo.grosjean@epitech.eu>
2023-06-13 16:36:50 +0200

    freelance card

--- src/components/Experiences.vue
+++ src/components/Experiences.vue
@@ -165,6 +165,87 @@
         </p>
       </div>
     </div>
+    <div id="card" class="card_freelance">
+      <svg id="sw-js-blob-svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+        <defs>
+          <clipPath id="shape">
+            <path
+              fill="#ffffff"
+              d="M6.6,-7.8C10.2,-9.3,16,-10.9,21.4,-9.6C26.9,-8.4,32,-4.2,35.4,2C38.9,8.2,40.6,16.3,36.9,20.5C33.1,24.6,23.7,24.8,16.7,24.8C9.6,24.8,4.8,24.7,1.4,22.3C-2,19.9,-4,15.2,-8.1,13.4C-12.2,11.7,-18.3,12.9,-22.7,11.1C-27.2,9.4,-30,4.7,-29.8,0.1C-29.7,-4.5,-26.6,-9.1,-25.2,-16C-23.7,-22.9,-23.8,-32.2,-19.9,-30.6C-16,-28.9,-8,-16.4,-3.3,-10.7C1.5,-5.1,2.9,-6.4,6.6,-7.8Z"
+              width="100%"
+              height="100%"
+              transform="translate(60 30)"
+              stroke-width="10"
+            ></path>
+          </clipPath>
+        </defs>
+        <image
+          x="-3"
+          y="-20"
+          width="100"
+          height="100"
+          scale="2"
+          clip-path="url(#shape)"
+          id="aliae_img"
+          href="@/assets/joomla.jpg"
+        />
+      </svg>
+      <p>Freelance</p>
+      <h1>Theo Grosjean It</h1>
+      <div class="list">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentcolor"
+          class="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+        <p>Migration d'un site Joomla vers la nouvelle version du cms</p>
+      </div>
+      <div class="list">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentcolor"
+          class="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+        <p>Création d'une nouvelle template fidèle a l'ancienne</p>
+      </div>
+      <div class="list">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentcolor"
+          class="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+        <p>
+          Réunions avec le client
+        </p>
+      </div>
+    </div>
   </div>
 
 </template>
@@ -277,12 +358,47 @@ svg {
   transition: all 0.2s ease-in;
   border-radius: 20px;
 }
+.card_freelance {
+  box-sizing: border-box;
+  flex-shrink: 0;
+  width: 600px;
+  border-bottom: #322765 solid 6px;
+  border-top: 1px solid #322765;
+  border-left: 1px solid #322765;
+  border-right: 4px solid #322765;
+  height: max-content;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  padding: 30px 30px 30px 30px;
+  background: linear-gradient(to left, #5a4ca0, #463b7b);
+  box-shadow: 4px 4px 10px rgba(66, 64, 72, 0.776);
+  overflow: hidden;
+  position: relative;
+  align-content: flex-start;
+  flex-wrap: nowrap;
+  transform: rotate3d(2, 0, 1, 48deg);
+  gap: 20;
+  transition: all 0.2s ease-in;
+  border-radius: 20px;
+}
+
 
 .card_lbp > svg {
   height: 200%;
   opacity: 0.07;
   position: absolute;
   overflow: visible;
+  background-size: 20px 30px;
+}
+.card_freelance > svg {
+  height: 200%;
+  filter: grayscale(1);
+  opacity: 0.07;
+  position: absolute;
+  overflow: visible;
+  transform: translateX(100px) translateY(10px) rotate(20deg);
   background-size: 20px 30px;
 }
 .card_aliae > svg {
@@ -297,6 +413,9 @@ svg {
   transform: rotate3d(2, 0, 1, 0deg) translateX(10px);
 }
 .card_lbp:hover {
+  transform: rotate3d(2, 0, 1, 0deg) translateX(10px);
+}
+.card_freelance:hover {
   transform: rotate3d(2, 0, 1, 0deg) translateX(10px);
 }
 h1 {
