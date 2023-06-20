983a84399764939089748c3f424f38506ed2a12a
Théo Grosjean <theo.grosjean@epitech.eu>
2023-06-20 19:07:23 +0200

    ortho

--- src/components/Experiences.vue
+++ src/components/Experiences.vue
@@ -224,7 +224,7 @@
             d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
           />
         </svg>
-        <p>Création d'une nouvelle template fidèle a l'ancienne</p>
+        <p>Création d'une nouvelle template fidèle à l'ancienne</p>
       </div>
       <div class="list">
         <svg
